92312b59889166b71fcdbbba4e316cfe1f40df9c
Kwasi Asante <mrasante@gmail.com>
2017-02-24 11:42:33 -0500

    added vector tile map

--- app/src/main/java/support/esri/com/fuse/MapActivity.java
+++ app/src/main/java/support/esri/com/fuse/MapActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
 public class MapActivity extends AppCompatActivity {
+
+    private ArcGISMap arcgisMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,11 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //create and add the map with basemap
+        MapView fuseMapView = (MapView)findViewById(R.id.fuse_map_view);
+        arcgisMap = new ArcGISMap(Basemap.Type.STREETS_NIGHT_VECTOR, -82.954, 42.298, 9);
+        fuseMapView.setMap(arcgisMap);
     }
 
 }
