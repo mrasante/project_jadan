4c5fbf6e85ea7a9500285e79d7c19d42f4ede0bd
Kwasi Asante <mrasante@gmail.com>
2017-02-24 11:12:56 -0500

    initial integration

--- app/src/main/java/support/esri/com/fuse/Launcher.java
+++ app/src/main/java/support/esri/com/fuse/Launcher.java
@@ -1,13 +1,24 @@
 package support.esri.com.fuse;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class Launcher extends AppCompatActivity {
+
+    private static final long DELAY_TIME = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =  new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        }, DELAY_TIME);
     }
 }
--- app/src/main/java/support/esri/com/fuse/LogInActivity.java
+++ app/src/main/java/support/esri/com/fuse/LogInActivity.java
@@ -1,8 +1,12 @@
 package support.esri.com.fuse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -11,7 +15,19 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Button closerButton = (Button) findViewById(R.id.closer);
+        closerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLauncherActivity();
+            }
+        });
+    }
 
+    private void startLauncherActivity() {
+        Intent intent = new Intent(getApplicationContext(), Launcher.class);
+        intent.putExtra("Annonymous", "ANNONYMOUS_USER");
+        startActivity(intent);
     }
 
 }
